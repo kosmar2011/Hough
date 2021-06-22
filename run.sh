1ceeaf97ff43bec08807dba8d3b1c8f9be8801f6
Kostas Marthoglou <kosmar2011@gmail.com>
2021-06-22 16:38:57 +0300

    added flags to vash

--- run.sh
+++ run.sh
@@ -3,20 +3,24 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"/build
 
-#Algorithm with Catapult
-#g++ -I /opt/mentor/catapult/Mgc_home/shared/include/ -I "$SCRIPT_DIR"/include -o "$SCRIPT_DIR"/build/main "$SCRIPT_DIR"/hough/bmp_io.cpp "$SCRIPT_DIR"/hough/Hough_tb.cpp
 
-#Algorithm
-g++ -I "$SCRIPT_DIR"/include/ -g -o "$SCRIPT_DIR"/build/main "$SCRIPT_DIR"/hough/bmp_io.cpp "$SCRIPT_DIR"/hough/HoughAlg_tb.cpp
+while getopts c: flag
+do
+    case "${flag}" in
+        c) use_catapult=${OPTARG};;
+    esac
+done
+
+if $use_catapult == true
+then
+    g++ -I /opt/mentor/catapult/Mgc_home/shared/include/ -I "$SCRIPT_DIR"/include -o "$SCRIPT_DIR"/build/main "$SCRIPT_DIR"/hough/bmp_io.cpp "$SCRIPT_DIR"/hough/Hough_tb.cpp
+    "$SCRIPT_DIR"/build/main "$SCRIPT_DIR"/images/canny.bmp "$SCRIPT_DIR"/images/outHLS.bmp
+else
+    g++ -I "$SCRIPT_DIR"/include/ -g -o "$SCRIPT_DIR"/build/main "$SCRIPT_DIR"/hough/bmp_io.cpp "$SCRIPT_DIR"/hough/HoughAlg_tb.cpp
+    "$SCRIPT_DIR"/build/main "$SCRIPT_DIR"/images/canny.bmp "$SCRIPT_DIR"/images/out.bmp
+fi
 
 #Algorithm CMD
 #g++ -I ./include/ -o ./build/main ./hough/bmp_io.cpp ./hough/Hough_tb.cpp
-
-#Run Build with Catapult
-# "$SCRIPT_DIR"/build/main "$SCRIPT_DIR"/images/canny.bmp "$SCRIPT_DIR"/images/outHLS.bmp
-
-#Run Build
-"$SCRIPT_DIR"/build/main "$SCRIPT_DIR"/images/canny.bmp "$SCRIPT_DIR"/images/out.bmp
-
 #Run Build CMD
 #./build/main ./images/canny.bmp ./images/out.bmp
